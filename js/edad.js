9c2ea7ecc1124cac6af1843e350f23f00361b910
Óscar <newoscardev@gmail.com>
2022-10-26 08:24:01 +1400

    Add files via upload

---
+++ js/edad.js
@@ -0,0 +1,9 @@
+function getAge(d1, d2){
+    d2 = d2 || new Date();
+    var diff = d2.getTime() - d1.getTime();
+    return Math.floor(diff / (1000 * 60 * 60 * 24 * 365.25));
+}
+
+document.getElementById('edad').innerHTML = getAge(new Date(2008, 11, 1));
+
+
